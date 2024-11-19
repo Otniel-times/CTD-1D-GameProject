23e1008299e2c4d4db4ac5ca9a5d5c7d289323a7
WeiXian042901 <wxian20@gmail.com>
2024-11-19 21:05:28 +0800

    basic stuffs

--- start.py
+++ start.py
@@ -4,4 +4,17 @@ print('You suck')
 import sqlite3
 
 connection = sqlite3.connect("index.db")
-print(connection.total_changes)
+
+cursor = connection.cursor()
+
+## create table command
+command = """CREATE TABLE IF NOT EXISTS
+test(test_id INTEGER PRIMARY KEY) """
+cursor.execute(command)
+
+cursor.execute("INSERT INTO <TABLE NAME> VALUES (...)")
+
+## get data
+cursor.execute("SELECT * FROM <TABLE>")
+results = cursor.fetchall()
+print(results)
