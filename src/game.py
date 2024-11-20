bfc51e5284ef3af95aefacc3a51d306b3274b8b8
gxy812 <56009756+gxy812@users.noreply.github.com>
2024-11-20 15:31:25 +0800

    Start game logic code

---
+++ src/game.py
@@ -0,0 +1,30 @@
+from enum import IntEnum
+
+# How to use:
+# Powerups.Anycubic for 0
+class Powerups(IntEnum):
+    Anycubic = 0
+    Bambu = 1
+    November = 31
+    
+
+class GameController:
+    def __init__(self) -> None:
+        # To store:
+        # Click count
+        # Powerups
+        self.coins = 0
+        # store as bitflags
+        self.powerups = 0
+    def earn(self):
+        self.coins += 1
+    def get_powerup(self, powerup: Powerups):
+        self.powerups |= 1 << powerup
+    def use_powerup(self, powerup: Powerups):
+        self.powerups &= ~(1 << powerup)
+    def get_data(self):
+        return self.coins, self.powerups
+    def generate_crisis(self):
+    #TODO
+        pass
+
