5dc2a579966ba40b2624b5cd8aa047d9ab5a0154
HighOnMelatonin <tanruianh@outlook.com>
2024-11-21 22:06:10 +0800

    Added basic handling functions

--- dbHandler.py
+++ dbHandler.py
@@ -5,15 +5,39 @@ connection = sqlite3.connect("index.db")
 cursor = connection.cursor()
 
 ## create table command
-def createTable():
-    command = """CREATE TABLE IF NOT EXISTS
-    test(test_id INTEGER PRIMARY KEY) """
+def createTable(tableName, priKey) -> None:
+    command = "CREATE TABLE IF NOT EXISTS{0}({1} INTEGER PRIMARY KEY)".format(
+        tableName, priKey)
+    
     cursor.execute(command)
 
-    cursor.execute("INSERT INTO <TABLE NAME> VALUES (...)")
-
 ## get data
-def getAll(tableName):
-    cursor.execute("SELECT * FROM {}",tableName)
+def getAll(tableName) -> list:
+    cursor.execute("SELECT * FROM ?",(tableName))
     results = cursor.fetchall()
     return results
+
+## add data
+def addData(tableName, data) -> None:
+    cursor.execute("INSERT INTO ? VALUES (?)", (tableName, data))
+
+## del one data entry
+def delOne(tableName, data) -> None:
+    cursor.execute("DELETE FROM ? WHERE")
+
+## Get specific data
+def getOne(tableName, queryType, query):
+    cursor.execute("SELECT ?  FROM ? WHERE ?", (queryType, tableName, query))
+
+
+## Commit changes
+def commit():
+    connection.commit()
+
+## Undo changes
+def undo():
+    connection.rollback()
+
+## Close
+def close():
+    connection.close()
