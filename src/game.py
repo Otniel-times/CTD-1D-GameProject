9d40312dae58b365870ad2c08aa4f5891fb2ce8e
gxy812 <56009756+gxy812@users.noreply.github.com>
2024-11-24 12:39:13 +0800

    Move dbHandler, begin integrating other code into game.py

--- src/game.py
+++ src/game.py
@@ -1,10 +1,13 @@
 from enum import IntEnum
+from gui import *
+import dbHandler
 
 # How to use:
 # Powerups.Anycubic for 0
 class Powerups(IntEnum):
     Anycubic = 0
     Bambu = 1
+    DouyinIonThrusters = 30
     November = 31
     
 
@@ -12,22 +15,29 @@ class GameController:
     def __init__(self) -> None:
         # To store:
         # Click count
-        self.coins = 0
+        self.score = 0
         # array of powerup counts
         # temporarily set to 32 entries until we decide how many we want
         # or if we want to use dictionary for this instead
         # but probably better as array for ease of entry into database
         self.powerups = [0] * 32
+        # TODO:implement userid
+        self.uid = 0
+        
+        self.gui = Main_GUI()
+        self.gui.clicker.config(command=self.earn)
+        # TODO: link to counter display once created
     def earn(self):
-        self.coins += 1
+        self.score += 1
     def get_powerup(self, powerup: Powerups):
         self.powerups[powerup] += 1
     def use_powerup(self, powerup: Powerups):
         # validation here? or maybe ui
         self.powerups[powerup] -= 1
-    def get_data(self):
-        return self.coins, self.powerups
+    def save(self):
+        dbHandler.update_score_by_id(self.uid, self.score)
     def generate_crisis(self):
     #TODO
         pass
 
+GameController()
