5a9004f5ba8527765df5708a8b46d21e9ebdc8b0
gxy812 <56009756+gxy812@users.noreply.github.com>
2024-11-30 16:25:11 +0800

    rename powerups

--- src/game.py
+++ src/game.py
@@ -5,16 +5,16 @@ import random
 
 # How to use:
 # Powerups.Anycubic for 0
-class Powerups(IntEnum):
+class Upgrades(IntEnum):
     Anycubic = 0
     Bambu = 1
-    DouyinIonThrusters = 30
-    November = 31
+    DouyinIonThrusters = 2
+    November = 15
     
 
 class GameController:
     def __init__(self) -> None:
-        self.POWERUP_COUNT = 32
+        self.UPGRADE_COUNT = 15
         self.ITEM_COUNT = 5
         
         self.rng = random.Random()
@@ -34,11 +34,14 @@ class GameController:
 
         self.CRISIS_COUNT = len(self.crises)
         # array of powerup counts
-        self.powerups = [0] * self.POWERUP_COUNT
+        self.upgrades = [0] * self.UPGRADE_COUNT
 
         # TODO: Write powerup actions
         # return new per click and per sec
-        self.powerup_actions = [lambda x,y: (0,0)] * self.POWERUP_COUNT
+        self.upgrade_actions = [
+            lambda x, y: (x+1, y+1), #Anycubic
+            lambda x, y: (x+5, y+5), #Bambu
+        ]
 
         self.items = [0] * self.ITEM_COUNT
 
@@ -77,14 +80,14 @@ class GameController:
         self.gui.score.set(self.score)
         self.gui.printer_head.animation_check()
 
-    def get_powerup(self, powerup: Powerups):
-        self.powerups[powerup] += 1
+    def get_powerup(self, powerup: Upgrades):
+        self.upgrades[powerup] += 1
 
-    def use_powerup(self, powerup: Powerups):
+    def use_powerup(self, powerup: Upgrades):
         # validation here? or maybe ui
-        self.powerups[powerup] -= 1
+        self.upgrades[powerup] -= 1
         self.prints_per_click, self.prints_per_sec = \
-            self.powerup_actions[powerup](self.prints_per_click, self.prints_per_sec)
+            self.upgrade_actions[powerup](self.prints_per_click, self.prints_per_sec)
 
 
     def save(self):
